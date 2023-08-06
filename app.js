adfdac02c9cd9a4692d485a3fb8d263e794e812e
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2023-08-06 17:01:42 +0530

    added a PORT variable

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use("/notes", notes);
-
+const PORT = process.env.PORT || 4000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(3000, console.log("Server is connected and started listening"));
+    app.listen(PORT, console.log("Server is connected and started listening"));
   } catch (err) {
     console.log(err);
   }
